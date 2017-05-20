5dc14131e53315426a0bbc3faeb87f1b8ff4997d
tanzhangyu <396818143@qq.com>
2017-05-20 17:28:39 +0800

    add one interviev test to the algorithmic_test/AlgorithmicTest.php

--- algorithmic/algorithmic_test/AlgorithmicTest.php
+++ algorithmic/algorithmic_test/AlgorithmicTest.php
@@ -12,7 +12,7 @@ class AlgorithmicTest {
         $this->robotSteps = 0;
     }
     // 递归方式爬楼梯
-    public static function _ascendStairs(int $n):int{
+    public  function _ascendStairs(int $n):int{
            switch ($n>1) {
                case 0:
                    return 1;
@@ -22,7 +22,7 @@ class AlgorithmicTest {
     }
 
     // 迭代爬楼梯
-    public static function _ascendStairsByIterator(float $n):float{
+    public  function _ascendStairsByIterator(float $n):float{
           $first = 1;
           $secend = 2;
           if($n<=2)
@@ -38,7 +38,7 @@ class AlgorithmicTest {
     }
 
     // 最大回文子窜
-    public static function _maxPalindromicStr(string $str){
+    public  function _maxPalindromicStr(string $str){
              $length = strlen($str);
              $arr = [];
              $start = 0 ;
@@ -66,7 +66,7 @@ class AlgorithmicTest {
     }
 
         // 最大回文子理论上这个方法效率高，实际上不如方法一
-    public static function _maxPalindromicStrTwo(string $str){
+    public  function _maxPalindromicStrTwo(string $str){
              $length = strlen($str);
              $arr = [];
              $start = 0 ;
@@ -100,7 +100,7 @@ class AlgorithmicTest {
     }
 
     // n个人有两个人同一天（365）生日的概率
-    public static function _sameBorthday(float $n):float{
+    public  function _sameBorthday(float $n):float{
         if($n<1)
             return 0;
         if($n>365)
@@ -116,7 +116,7 @@ class AlgorithmicTest {
   
     // 全排列 for循环
     // $arrange = ['location'=>value];
-    public static function _arrangeLocationToValue(array $arr){
+    public  function _arrangeLocationToValue(array $arr){
           $length = count($arr);
           if($length<2)return;
           $n = self::_factorial($length);
@@ -138,7 +138,7 @@ class AlgorithmicTest {
     // 排几个数时比for循环的性能好一丢丢
     // $arrange = ['location'=>value];
     // 返回$arrange;
-        public static function _arrangeLocationToValueW(array $arr):array{
+        public  function _arrangeLocationToValueW(array $arr):array{
           $length = count($arr);
           if($length<2)return $arr;
           $arrange = [];
@@ -158,7 +158,7 @@ class AlgorithmicTest {
     }
 
     // 全逆序
-    public static function _opposite(array $arr):array{
+    public  function _opposite(array $arr):array{
         $end = count($arr)-1;
         for ($i = 0,$j = $end; $i < $j; ++$i,--$j) { 
             $tmp = $arr[$i];
@@ -169,20 +169,20 @@ class AlgorithmicTest {
     }
 
     // 子数组逆序
-    public static function _reverse(array &$array,int $start,int $end){
+    public  function _reverse(array &$array,int $start,int $end){
             for ($i=$start,$j = $end; $i < $j ; ++$i,--$j) { 
                   self::_swap($array[$i],$array[$j]);
             }
     }
 
    // 交换
-    public static function _swap(&$a,&$b){
+    public  function _swap(&$a,&$b){
         $tmp = $a;
         $a = $b;
         $b = $tmp;
     }
 // 阶乘
-    public static function _factorial(int $n):int{
+    public  function _factorial(int $n):int{
         for($i = $n,$sum = 1;$n>1;--$n){
               $sum*=$n;
         }
@@ -192,7 +192,7 @@ class AlgorithmicTest {
    
      // 全排列
     // $arrange = ['value'=>location];
-    public static function _arrangeValueToLocation(array $arr):array{
+    public  function _arrangeValueToLocation(array $arr):array{
         $arr = self::_arrangeLocationToValueW($arr);
         $arrange = [];
         $arrangeChild = []; 
@@ -206,7 +206,7 @@ class AlgorithmicTest {
     }
 
      // 某一道概率问题：1,2,3,4,5;4 不能在中间，5和3不能在一起
-    public static function _someQuestionOfProbability(array $arr):array{
+    public  function _someQuestionOfProbability(array $arr):array{
         $arrange = self::_arrangeValueToLocation($arr);
         $oldTotal = count($arrange);
         foreach ($arrange as $key => $value) {
@@ -221,7 +221,7 @@ class AlgorithmicTest {
     }
 
     //创建回文数
-public static function createStr(int $len,int $min=0,int $max=9):string{
+public  function createStr(int $len,int $min=0,int $max=9):string{
     $str = '';
     for ($i=0, $k = $min,$turn = true; $i < $len; ++$i) { 
         $str.= $k;
@@ -243,7 +243,7 @@ public static function createStr(int $len,int $min=0,int $max=9):string{
 }
 
     // 实现标准库strpos()
-    public static function strpos(string $haystack,$needle,int $offset = 0):int{
+    public  function strpos(string $haystack,$needle,int $offset = 0):int{
           $needle = (string)$needle;
           $lengthOfNeedle = strlen($needle);
           $lengthOfHaystack = strlen($haystack);
@@ -264,7 +264,7 @@ public static function createStr(int $len,int $min=0,int $max=9):string{
          return -1;
     }
    //实现标准库strstr()
-    public static function strstr(string $haystack,$needle,bool$before_needle = false){
+    public  function strstr(string $haystack,$needle,bool$before_needle = false){
           $point = self::strpos($haystack,$needle);
           if($point<0) return false; 
           $string = '';
@@ -306,7 +306,7 @@ public static function createStr(int $len,int $min=0,int $max=9):string{
      * @return [array]       [the result of the searching]
      * 返回一个文件里某个单词的信息，包括总数，位置
      */
-     public static function findWordByPreg($find,$path){
+     public  function findWordByPreg($find,$path){
        $file = fopen($path, 'r');
        $line = 1;
        $count = 0;
@@ -511,7 +511,7 @@ public static function createStr(int $len,int $min=0,int $max=9):string{
       * @return [array]              [the result of combination ,include each ways detail and numbers of way]
       * this way is more effect than the combinationsByTransform($allElements,int $selectMany)
       */
-     public static function combinations(array $allElements,int $selectMany){
+     public  function combinations(array $allElements,int $selectMany){
           $length = count($allElements);
           if($selectMany<1) return false;
           if($selectMany==1) return $allElements;
@@ -559,7 +559,7 @@ public static function createStr(int $len,int $min=0,int $max=9):string{
            * @param  int    $selectMany  [description]
            * @return [type]              [description]
            */
-          public static function combinations2(array $allElements,int $selectMany){
+          public  function combinations2(array $allElements,int $selectMany){
           $length = count($allElements);
           if($selectMany<1) return false;
           if($selectMany==1) return $allElements;
@@ -617,7 +617,7 @@ public static function createStr(int $len,int $min=0,int $max=9):string{
       * @param  int    $selectMany  [description]
       * @return [type]              [description]
       */
-     public static function combinationsByTransform(array$allElements,int$selectMany){
+     public  function combinationsByTransform(array$allElements,int$selectMany){
             $count = count($allElements);
             $combination = [];  //each combination
             for ($i=0; $i < $count; ++$i) { 
@@ -656,24 +656,164 @@ public static function createStr(int $len,int $min=0,int $max=9):string{
             }
          return $combinations;
      }
+     
+     // 该方法可正确计算的条件是：$sum不存在重复值
+     // 时间：N 空间 N
+     public function divGroup(array$array){
+       $length = count($array);
+       for ($i=0,$sum = 0,$sums = []; $i < $length; ++$i) { 
+           $sum += $array[$i];
+           $sums[$sum] = $i;
+       }
+       if(isset($sums[$sum/4]) && isset($sums[$sum/4*2]) && isset($sums[$sum/4*3]))
+           echo $sums[$sum/4],' ',$sums[$sum/4*2],' ',$sums[$sum/4*3];
+       else
+           echo 'it is not the available array to div into 4 equal';
+     }
+     
+     /**
+      * [div2pairs divide the array into two group ]
+      * @param  int    $avg        [dynamic average]
+      * @param  int    $ldeliIndex [the left delimeter]
+      * @param  array  &$array     [the array to divide by lsum compared rsum]
+      * @param  int    $rdeliIndex [the right delimeter]
+      * @return [mixed]             [if true return the middle delimeter,or false]
+      */
+     public function div2pairs(int $avg,int $ldeliIndex,array&$array,int $rdeliIndex){
+          $lstart = $ldeliIndex+1;  //the index where begin to calculate the sum from left;
+          $rstart = $rdeliIndex-1;  //the index where begin to calculate the sum from right;
+          $lsum = $array[$lstart]; //init the $lsum
+          $rsum = $array[$rstart]; //init the $rsum
+          $gap = $lsum-$rsum;
+          while($lstart<$rstart){   //although within two 'while' but its time complexity is truely o(n); 
+             while($lsum>$rsum && $lstart<$rstart-2){ //here the number 2 means that there must be 
+                    $rsum+=$array[--$rstart];                 //a delimeter index can't not put into calculate
+                    if($lsum-$rsum>$gap && $lstart<$rstart-2){ //avoid the gap being bigger;
+                         $lsum+=$array[++$lstart]; 
+                    }                                   
+             }
+             while ($lsum<$rsum && $lstart<$rstart-2) {
+                    $lsum+=$array[++$lstart]; 
+                    if($lsum-$rsum<$gap && $lstart<$rstart-2){
+                         $rsum+=$array[--$rstart];
+                    }
+                    $gap = $lsum-$rsum;
+             }
+             if($rsum==$lsum){
+                 if($rstart ==$lstart+2){
+                     if($rsum ==$avg) //true return the dividing delimeter 
+                      return $lstart+($rstart-$lstart)/2;
+                     else
+                      return false;   
+                 }else{
+                    ++$lstart; //you can also change these two lines like this:
+                     $lsum+=$array[$lstart];  //--$rstart;$rsum+=$array[$rstart]
+                 }
+             }else{
+                    if($lsum<$rsum){ //here and 'while' is used to make the $lsum and $rsum more close;
+                      ++$lstart;
+                     $lsum+=$array[$lstart];
+                    }else{
+                      --$rstart;
+                      $rsum +=$array[$rstart];
+                    } 
+              }
+           $gap = $lsum-$rsum;  //upgrade the gap between $lsum and $rsum;
+          }
+          return false;
+     }
+      /**
+       * [div4pairs divide the array into 4 pairs with delimeter]
+       * @param  array  $array [array to divide]
+       * @return [mixed]        [if true then return an array cluding all delimeter]
+       */
+     public function div4pairs(array$array){
+         $length = count($array); //get the lengt of the array;
+         if($length<5) return false; //because divide into 4 equals with delimeter
+         
+         $lsumArray = []; //array to store the lsum which calculate from 0;
+         $lsumArray[0][] = -1; //$lsumArray[$lsum] = index, if index<0 ,default -1 
+         $end = $length-2; //the 4th pair must be >=0,default $array[$length] = 0 as well as the 
+                                     //$array[-1] = 0,thought they do not exist
+         for ($i = 0,$lsum = 0; $i < $end; ++$i) {  
+             $lsum+=$array[$i];      
+             $lsumArray[$lsum][] = $i;  //store the index for each lsum;
+         }
+         for($j = $length-1,$rsum = 0;$j>3;--$j){ //from right to left ,calculate the rsum;
+            if(isset($lsumArray[$rsum])){         //
+               while(false!==current($lsumArray[$rsum])){
+                  $ldeliIndex = current($lsumArray[$rsum])+1;
+                //  if($ldeliIndex>$j-1) break;   //3 elements between ldelimeter and rdelimeter($j);
+                  if(($mid=self::div2pairs($rsum,$ldeliIndex,$array,$j))) //try to divide array into 4 equals
+                    return [$ldeliIndex,$mid,$j]; //true then return these three delimeters index;
+                 next($lsumArray[$rsum]); 
+               }
+            }
+            $rsum+=$array[$j]; 
+         }
+        return false;
+     }
+     /**
+      * [div4pairsF another simple method to divide array into 4 equals with excluding delimeter index]
+      * @param  array  $array [array to divide]
+      * @return [mixed]        [if true return delimeters index,or false]
+      */
+     public function div4pairsF(array$array){
+          $length = count($array);
+          $ilength = $length-4;
+          $jlength = $length-2;
+          for ($i=0,$isum = 0;$i < $ilength; ++$i) { 
+              for ($j=$i+1,$jsum = 0; $j < $jlength; ++$j) { 
+                      $jsum+=$array[$j];
+                      if($jsum==$isum){
+                        ++$j;
+                          for($k = $j+1,$ksum = 0;$k<$length;++$k){
+                              $ksum+=$array[$k];
+                              if($ksum==$isum){
+                                 ++$k;
+                                  if($k+1<$length){
+                                     for ($n=$k+1,$nsum=0; $n < $length; ++$n) { 
+                                         $nsum+=$array[$n];
+                                     }
+                                     if($nsum==$isum) 
+                                       return [$i,$j,$k];
+                                  }
+                              }
+                          }
+                      }
+              }
+              $isum+=$array[$i];
+          }
+          return false;
+     }
+
+         public function createTestingData(int $m){
+             for ($i=0,$data=[]; $i < $m; ++$i) { 
+                 $in = mt_rand(-$m,$m);
+                    $data[] = $in;
+            }
+            return $data; 
+         }
+      
+
  }  
 
- $all = [];
- $m = 20;
- $n = 10;
- for ($i=0; $i < $m; ++$i) { 
-    $all[] = $i;
- }
+// $n = 1000000;//1000,10000,100000,1000000
+// $all = AlgorithmicTest::createTestingData($n);
+// $all = [1,2,5,4,-1];  // false
+$all = [1,-1,2,-1,4,-3,4,2,1];
 echo memory_get_usage(),'<br>';
 $s = microtime(true);
-// $arr =AlgorithmicTest::combinationsByTransform($all,$n);
-$arr =AlgorithmicTest::combinations2($all,$n);
+$res = AlgorithmicTest::div4pairs($all); 
+// $res = AlgorithmicTest::div4pairsF($all);
 echo 'times:      ',microtime(true)-$s,'<br>';
 echo memory_get_peak_usage(),'<br>';
-echo 'counts:',count($arr);
-// echo '<pre>';
-// print_r($arr);
-// echo '</pre>';
+
+print_r($res);
+
+
+
+
 
 
 
